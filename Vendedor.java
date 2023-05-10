096d81c22773134a7d9f00049a82e3377efc3375
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-05-10 17:09:10 +0000

    salvar

---
+++ Vendedor.java
@@ -0,0 +1,24 @@
+public class Vendedor extends Empregado{
+    
+    private float percentualComissao;
+
+    public void setPercentualComissao(float percentualComissao) {
+        this.percentualComissao = percentualComissao;
+    }
+
+    public float getPercentualComissao() {
+        return percentualComissao;
+    }
+
+    public Vendedor(){
+
+    }
+
+    public Vendedor(String nome, float salario, float percentualComissao) {
+        super(nome, salario);
+        this.percentualComissao = percentualComissao;
+    }
+
+
+
+}
